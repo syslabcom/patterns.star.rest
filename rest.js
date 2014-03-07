205a6c39499424481497be9812400b32e4b56bea
Alexander Pilz <pilz@syslab.com>
2014-03-07 16:25:03 +0100

    new

---
+++ rest.js
@@ -0,0 +1,89 @@
+/**
+ * Patterns rest - Interact with restful apis
+ *
+ * Copyright 2012-2013 Florian Friesdorf
+ * Copyright 2012-2013 Marko Durkovic
+ */
+define([
+    "jquery",
+    "registry",
+    "logger",
+    "parser",
+    "star-ajax"
+], function($, registry, logger, Parser, ajax) {
+    var log = logger.getLogger('pat.rest'),
+        parser = new Parser('rest');
+
+    var _ = {
+        name: "rest",
+        trigger: "form.pat-rest",
+        parser: parser,
+        init: function($el) {
+            $el.on('submit.pat-rest', _.onSubmit)
+               .on('click.pat-ajax', '[type=submit]', _.onClickSubmit);
+            return $el;
+        },
+        destroy: function($el) {
+            $el.off('.pat-rest');
+        },
+        onSubmit: function(event) {
+            if (event) {
+                event.preventDefault();
+            }
+            _.request($(this));
+        },
+        onClickSubmit: function(event) {
+            var $form = $(event.target).parents('form').first(),
+                name = event.target.name,
+                value = $(event.target).val(),
+                data = [];
+            if (name) {
+                data.push({name: name, value: value});
+            }
+            $form.data('pat-rest.clicked-data', data);
+        },
+        request: function($el, opts) {
+            return $el.each(function() {
+                _._request($(this), opts);
+            });
+        },
+        _serializeObject: function($el) {
+            var ret = {};
+            $.each($el.serializeArray()
+                      .concat($el.data('pat-rest.clicked-data') || []),
+                     function() {
+                if (this.value === '') {
+                    return;
+                }
+                if ($('[name="'+this.name+'"]')
+                    .parents('.hidden,.disabled').length > 0) {
+                    return;
+                }
+
+                if (ret[this.name]) {
+                    if (!ret[this.name].push) {
+                        ret[this.name] = [ret[this.name]];
+                    }
+                    ret[this.name].push(this.value || '');
+                } else {
+                    ret[this.name] = this.value || '';
+                }
+            });
+            return ret;
+        },
+        _request: function($el, opts) {
+            var args = {
+                contentType: 'application/json',
+                context: $el,
+                data: JSON.stringify(_._serializeObject($el)),
+                dataType: 'json',
+                type: $el.attr('method'),
+                url: $el.attr('action')
+            };
+            ajax.request(args);
+        }
+    };
+
+    registry.register(_);
+    return _;
+});
